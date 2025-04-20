8d3f16adcf6e83d8ced9ef826de1159c94a811d9
Mradbot_Admin <76399884+shai1995@users.noreply.github.com>
2025-04-20 20:59:23 +0530

    Update var.py

--- var.py
+++ var.py
@@ -7,7 +7,7 @@ API_HASH = os.getenv("API_HASH", "")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 ADMIN = int(os.getenv("ADMIN", ""))
 
-CHNL_LINK = os.getenv("CHNL_LINK", "https://t.me/DypixxTech")
+CHNL_LINK = os.getenv("CHNL_LINK", "https://t.me/MRADBOT_OFFICIALS")
 LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-100xxxxxxx"))
 DUMP_CHANNEL = int(os.getenv("DUMP_CHANNEL", "-100xxxxxxx"))
 
@@ -15,7 +15,7 @@ DB_URI = os.getenv("DB_URI", "") #MongoDB URL
 DB_NAME = os.getenv("DB_NAME", "DypixxTech")
 
 IS_FSUB = bool(os.environ.get("FSUB", True)) # Set "True" For Enable Force Subscribe
-AUTH_CHANNEL = int(os.environ.get("AUTH_CHANNEL", "-100xxxxxxx"))
+AUTH_CHANNEL = int(os.environ.get("AUTH_CHANNEL", "-1001315120330"))
 
 REEL_AUTO_DELETE = int(os.getenv("REEL_AUTO_DELETE", "600")) #10min
 
