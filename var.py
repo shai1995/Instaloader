df53a99579bc4ad74122e7e7ac99934facaa7f52
Mradbot_Admin <76399884+shai1995@users.noreply.github.com>
2025-04-26 22:24:08 +0530

    Update var.py

--- var.py
+++ var.py
@@ -8,14 +8,14 @@ BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 ADMIN = int(os.getenv("ADMIN", ""))
 
 CHNL_LINK = os.getenv("CHNL_LINK", "https://t.me/MRADBOT_OFFICIALS")
-LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-100xxxxxxx"))
-DUMP_CHANNEL = int(os.getenv("DUMP_CHANNEL", "-100xxxxxxx"))
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1002564857492"))
+DUMP_CHANNEL = int(os.getenv("DUMP_CHANNEL", "-1002528883374"))
 
 DB_URI = os.getenv("DB_URI", "") #MongoDB URL
-DB_NAME = os.getenv("DB_NAME", "DypixxTech")
+DB_NAME = os.getenv("DB_NAME", "Cluster0")
 
 IS_FSUB = bool(os.environ.get("FSUB", True)) # Set "True" For Enable Force Subscribe
-AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-100xxxxxxxxx -100xxxxxxx").split())) # Add Multiple channel id
+AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-1001315120330").split())) # Add Multiple channel id
 
 REEL_AUTO_DELETE = int(os.getenv("REEL_AUTO_DELETE", "600")) #10min
 
