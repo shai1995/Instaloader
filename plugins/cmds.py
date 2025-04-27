a88b19ff39090785a12b803d792fc062bbce7d47
Mradbot_Admin <76399884+shai1995@users.noreply.github.com>
2025-04-27 16:13:50 +0530

    Update cmds.py

--- plugins/cmds.py
+++ plugins/cmds.py
@@ -21,9 +21,7 @@ async def start_cmd(client, message):
     if IS_FSUB and not await get_fsub(client, message):return
     await message.reply_text(txt.START_TXT.format(message.from_user.mention),
                              reply_markup=InlineKeyboardMarkup([
-                                 [InlineKeyboardButton("🎭 Updates 🎭", url="https://telegram.me/MRADBOT_OFFICIALS")],
-                                 [InlineKeyboardButton("⚡ Help", callback_data="help"), InlineKeyboardButton("📚 About", callback_data="about")],
-                                 [InlineKeyboardButton("🧑‍💻 Developer", user_id=int(ADMIN))]]))
+                                 [InlineKeyboardButton("🎭 Updates 🎭", url="https://telegram.me/MRADBOT_OFFICIALS")]]))
 
 @Client.on_message(filters.command("broadcast") & (filters.private) & filters.user(ADMIN))
 async def broadcasting_func(client: Client, message: Message):
